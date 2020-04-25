3ef7c63d2ce50f38cfe20a21c4a8b0cb632163a9
Muhammed Ahmed <muhammad.farghali14@gmail.com>
2020-04-25 17:15:40 +0200

    learning

---
+++ clone-real-world-vue/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Event List</router-link> |
+      <router-link to="/event-create">Create New Event</router-link>
+      <router-link to="/event-show/1">Event One</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style></style>
