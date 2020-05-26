af7dca1adb1bc41ad99cbad31039c70751505984
muhammed-farghali <muhammad.farghali14@gmail.com>
2020-05-26 22:08:22 +0200

    ..

--- clone-real-world-vue/src/App.vue
+++ clone-real-world-vue/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Event List</router-link> |
-      <router-link to="/event-create">Create New Event</router-link>
+      <router-link to="/event-create">Create New Event</router-link> |
       <router-link to="/event-show/1">Event One</router-link>
     </div>
     <router-view />
